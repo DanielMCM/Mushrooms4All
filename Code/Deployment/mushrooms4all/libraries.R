f7a2adadea364549ed7cf05eea7f4ee047d976ca
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2018-12-14 22:21:58 +0100

    Load libraries dynamically using requirements.txt file

--- Code/Deployment/mushrooms4all/libraries.R
+++ Code/Deployment/mushrooms4all/libraries.R
@@ -2,20 +2,34 @@
 ##########           Libraries     ################
 ###################################################
 
-library(shiny)
-library(shinyjs)
-library(shinydashboard)
-library(shinythemes)
-library(stringr)
-library(arules)
-library(dplyr)
-library(gridExtra)
-library(grid)
-library(dplyr)
-library(ggplot2)
-library(lattice)
-library(arulesViz)
-library(utils)
-library(lubridate)
-library(rlist)
-library(lubridate)
+load_libraries <- function(filepath) {
+
+    # read file with packages
+
+    i <- 1
+    to_install <- list()
+    con = file(filepath, "r") # open file connection
+    while (TRUE) {
+        line = readLines(con, n = 1)
+        if (length(line) == 0) {
+            break
+        }
+        to_install[[i]] <- line
+        i <- i + 1
+    }
+
+    close(con) # close connection
+
+    # install and load packages
+
+    for (package in to_install) {
+        if (!is.element(package, .packages(all.available = TRUE))) {
+            install.packages(package)
+        }
+        library(package, character.only = TRUE)
+    }
+}
+
+# load required libraries
+
+load_libraries("requirements.txt")
