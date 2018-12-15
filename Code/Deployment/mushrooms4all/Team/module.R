8da08c0b28bb730f6e32c73dcbae2b05de31878d
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2018-12-15 02:09:27 +0100

    Fix incomplete_string

--- Code/Deployment/mushrooms4all/Team/module.R
+++ Code/Deployment/mushrooms4all/Team/module.R
@@ -11,10 +11,8 @@ team_ui <- function(id) {
     tabPanel(
         title = "Team",
         fluidRow(class = "team-content",
-            box(class = "team-profile", img(src = 'Team member 1.png', align = "center", class = "team-profile-img"), h3("Daniel Minguez Camacho")),
-            box(class = "team-profile", img(src = 'Team member 2.jpg', align = "center", class = "team-profile-img"), h3("Javier de la Rúa Martínez"))
-        )
-    )
+            box(class = "team-profile", img(src = "Team member 1.png", align = "center", class = "team-profile-img"), h3("Daniel Minguez Camacho")),
+            box(class = "team-profile", img(src = "Team member 2.jpg", align = "center", class = "team-profile-img"), h3("Javier de la Rua Martinez"))))
 }
 
 # Server
